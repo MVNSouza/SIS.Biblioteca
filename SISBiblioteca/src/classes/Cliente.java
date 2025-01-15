e323cc7f69b39c634f431df0e4ba442bae0893db
Marcos V. N. Souza <marcos.nasouza2@gmail.com>
2025-01-15 20:36:06 -0300

    Create superclass Cliente

---
+++ SISBiblioteca/src/classes/Cliente.java
@@ -0,0 +1,14 @@
+package classes;
+
+public class Cliente {
+    protected String nome, id, senha, email;
+
+    public void login(String email, String senha){
+        
+    }
+
+    public void autocadastro(String email, String senha){
+
+    }
+
+}
