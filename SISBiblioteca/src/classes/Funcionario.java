60629d218bd5303bfca6cdbf2d6a3e2b56909f5b
Marcos V. N. Souza <marcos.nasouza2@gmail.com>
2025-01-21 08:24:49 -0300

    Update Funcionario.java

--- SISBiblioteca/src/classes/Funcionario.java
+++ SISBiblioteca/src/classes/Funcionario.java
@@ -2,10 +2,14 @@ package classes;
 public class Funcionario extends Cliente {
     boolean solicitacoesPendentes;
 
-    public Funcionario(String nome, String id, String senha, String email){
+    public Funcionario(String nome, int id, String senha, String email){
         this.nome = nome;
         this.id = id;
         this.senha = senha;
         this.email = email;
+    }
+
+    public String getNome(){
+        return nome;
     }
 }
