0c04d4ff422c759dad255ede4f12688ecef0fccd
Marcos V. N. Souza <marcos.nasouza2@gmail.com>
2025-01-22 08:16:24 -0300

    Merge conflict resolvido, teste controlador
    Resolvido os conflitos na classe funcionário e mudança no seu construtor com base nas mudanças em Cliente.

--- SISBiblioteca/src/classes/Controlador.java
+++ SISBiblioteca/src/classes/Controlador.java
@@ -4,8 +4,10 @@ import java.util.Scanner;
 
 public class Controlador {
     public static Scanner input = new Scanner(System.in);
-    static Funcionario f = new Funcionario("admin", 1, "123", "marcos@email.com");
+    static Funcionario f = new Funcionario("admin", "123", "marcos@email.com");
     static Cliente.Login l = new Cliente.Login("marcos@email.com", "123");
+
+
     
     
     public static void menuInicial(){
@@ -18,7 +20,11 @@ public class Controlador {
     }
 
     public static void menuFuncionario(Funcionario func){
+        System.out.println("Cadastrar: \n0- Usuário\n1- Funcionário");
+        int opcao = input.nextInt();
+        Cliente.cadastro(opcao);
 
+        
     }
 
     public static void menuUsuario(Usuario user){
